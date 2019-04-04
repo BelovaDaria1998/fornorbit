55b8730d016ea172fca39b92afaa69ab0783c21c
belovattt <belovatttt@gmail.com>
2019-04-04 14:23:18 +0300

    Initial commit

---
+++ src/main/java/com/belova/fornorbit/FornorbitApplication.java
@@ -0,0 +1,13 @@
+package com.belova.fornorbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornorbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FornorbitApplication.class, args);
+	}
+
+}
